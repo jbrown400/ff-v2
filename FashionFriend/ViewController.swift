18b197fd8ac68449db4aa3a450cdd8f974c44a7d
jbrown400 <justin.brown400@gmail.com>
2020-08-27 11:53:17 -0500

    Created project and added .gitignore.

---
+++ FashionFriend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FashionFriend
+//
+//  Created by Justin Brown on 8/27/20.
+//  Copyright © 2020 Justin Brown. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
